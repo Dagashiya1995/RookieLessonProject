fae8580a21c39d4e89db6b9d8a81ef47c147dfa1
Dagashiya1995 <touroku146749@gmail.com>
2017-03-09 00:20:07 +0900

    Implements to delete and edit of SubjectPage model

--- app/controllers/subject_pages_controller.rb
+++ app/controllers/subject_pages_controller.rb
@@ -7,6 +7,16 @@ class SubjectPagesController < ApplicationController
   end
 
   def edit
+    @subject_page = SubjectPage.find(params[:id])
+  end
+
+  def update
+    subject_page = params['subject_page']
+    SubjectPage.update(title: subject_page['title'],
+                       category: subject_page['category'] ,
+                       body: subject_page['body'],
+                      )
+    redirect_to subject_pages_path
   end
 
   def new
@@ -19,6 +29,12 @@ class SubjectPagesController < ApplicationController
                        category: subject_page['category'] ,
                        body: subject_page['body'],
                       )
+    redirect_to subject_pages_path
+  end
+
+  def destroy
+    subject_page = SubjectPage.find(params[:id])
+    subject_page.destroy
     redirect_to subject_pages_path
   end
 end
