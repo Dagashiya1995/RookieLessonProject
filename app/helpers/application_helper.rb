3bc680aa8d045ab3c9d3cb6d732869c630dd4a74
ueyuki <touroku146749@gmail.com>
2017-05-17 22:47:23 +0900

    Implemnt Markdown output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div(:line_numbers => :table)
+    end
+  end
+
+  @@coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
+  @@redcarpet = Redcarpet::Markdown.new(@@coderayified, {
+    :fenced_code_blocks => true,
+    :no_intra_emphasis  => true,
+    :autolink           => true,
+    :strikethrough      => true,
+    :lax_html_blocks    => true,
+    :superscript        => true
+  })
+
+  def markdown(text)
+    @@redcarpet.render(text).html_safe
+  end
+
 end
